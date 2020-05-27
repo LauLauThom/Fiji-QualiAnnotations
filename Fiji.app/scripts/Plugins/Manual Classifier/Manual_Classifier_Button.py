72fdcff497063bc7122845c0361538c3ca07cc65
Laurent Thomas <laurent132.thomas@laposte.net>
2020-05-27 18:35:21 +0200

    Button plugin with new class system

--- Fiji.app/scripts/Plugins/Manual Classifier/Manual_Classifier_Button.py
+++ Fiji.app/scripts/Plugins/Manual Classifier/Manual_Classifier_Button.py
@@ -1,116 +1,132 @@
-#@ int (Label = "Number of categories", min=1) N_category 
-#@ PrefService pref
-#@ ImageJ ij
+#@ int (Label = "Number of categories", min=1) N_category  
+#@ PrefService pref 
+#@ ImageJ ij 
 '''
-This script can be used to manually classify full images from a stack into N user-defined categories. 
-A first window pops up to request the number of categories. 
-A second window follows asking for the name to use for those categories. 
-Finally a third window will show up with one button per category. 
-Clicking on the button will generate a new entry in a table with the image name and the category. 
-It will also skip to the next slice for stacks. 
+This script can be used to manually classify full images from a stack into N user-defined categories.  
+A first window pops up to request the number of categories.  
+A second window follows asking for the name to use for those categories.  
+Finally a third window will show up with one button per category.  
+Clicking on the button will generate a new entry in a table with the image name and the category.  
+It will also skip to the next slice for stacks.  
 '''
-from ij	            import IJ, WindowManager
-from ij.measure 	import ResultsTable, Measurements
-from ij.plugin.filter import Analyzer
-from ij.gui		    import GenericDialog, NonBlockingGenericDialog 
-from java.awt.event import ActionListener 
-from java.awt 		import GridLayout, Button, Panel
-from QualiAnnotations import CustomDialog, getTable, ButtonAction
-import os 
+from ij	            import IJ, WindowManager 
+from ij.measure 	import ResultsTable, Measurements 
+from ij.plugin.filter import Analyzer 
+from ij.gui		    import GenericDialog, NonBlockingGenericDialog  
+from java.awt.event import ActionListener  
+from java.awt 		import GridLayout, Button, Panel 
+from QualiAnnotations import CustomDialog, getTable, ButtonAction 
+import os  
 
-
-
-class CustomAction(ButtonAction): # extend ButtonAction to inherit the actionPerformed method
+class CatButtonAction(ButtonAction): 
+	'''Define what happens when a button is clicked'''
 	
-	def __init__(self, dialog, cat):
-		ButtonAction.__init__(self, dialog)
-		self.cat=cat
-	
-	def fillFunction(self, Table):
-		if choiceIndex==0: # single category column
-			Table.addValue("Category", self.cat)
-
-		else: # 1 column/category with 0/1
-			for cat in listCat: 
-				if cat == self.cat: 
-					Table.addValue(cat, 1) 
-				else: 
-					Table.addValue(cat, 0)
+	def actionPerformed(self, event): 
+		'''Update the selected category and doAction to fill the table'''
+		self.dialog.selectedCategory = event.getSource().getLabel() 
+		self.dialog.doAction() 
 		 
-############### GUI - CATEGORY DIALOG - collect N classes names (N define at first line)  ############# 
  
-Win = GenericDialog("Categories names")
-
-choice = ["a single category column", "1 column per category"]
-indexDefault = pref.getInt("table_style", 0)
-Win.addChoice("Classification table shoud have", 
-				choice, 
-				choice[indexDefault] )
- 
-# Add N string field to get class names
-for i in range(N_category):
-	listCat = pref.getList(ij.class, "listCat")            # try to retrieve the list of categories from the persistence, if not return [] - ij.class workaround see https://forum.image.sc/t/store-a-list-using-the-persistence-prefservice/26449
+class ButtonDialog(CustomDialog): 
+	'''Annotation dialog, also define keyboard shortcut and function to fill the table'''
 	
-	if listCat and i<=len(listCat)-1:
-		catName = listCat[i]
-	else:
-		catName = "Category_" + str(i+1)
-	
-	Win.addStringField("Category: ", catName)
-	
-	Win.addMessage("") # skip one line 
+	def __init__(self, title, choiceIndex): 
+		CustomDialog.__init__(self, title) 
+		self.choiceIndex = choiceIndex 
+		self.selectedCategory = "" 
 	 
-Win.showDialog()
+	def fillTable(self, table): 
+		if self.choiceIndex==0: # single category column 
+			table.addValue("Category", self.selectedCategory) 
  
- 
-################# After OK clicking ########### 
- 
-# Recover fields from the formular 
-if (Win.wasOKed()):  
-
-	# get Choice single/multi column
-	choiceIndex = Win.getNextChoiceIndex()
-	pref.put("table_style", choiceIndex)
-	
-	tableTitle, Table = getTable()
+		else: # 1 column/category with 0/1 
+			for cat in listCat:  
+				if cat == self.selectedCategory:  
+					table.addValue(cat, 1)  
+				else:  
+					table.addValue(cat, 0) 
 	 
-	# Initialize GUI with category buttons 
-	WinButton = NonBlockingGenericDialog("Manual classifier - Single class per image") 
-	WinButton.addMessage("Click the category of the current image or ROI.\nTo annotate ROI, draw a ROI or activate one before clicking the category button.")
-		
-	# Loop over categories and add a button to the panel for each
-	listCat = [] 
-	catPanel = Panel(GridLayout(0,4)) # Unlimited number of rows - fix to 4 columns
-	
-	for i in range(N_category): 
+	def keyPressed(self, keyEvent): 
+		'''Check if the key pressed match any of the category''' 
 		 
-		# Recover the category name 
-		Cat = Win.getNextString() 
-		listCat.append(Cat) 
- 
-		# Create an instance of button action for this category 
-		#Action = ButtonAction(Cat) 
-		Action = CustomAction(WinButton, Cat)
- 
-		# Create a Button 
-		button = Button(Cat) 
-		 
-		# Bind action to button 
-		button.addActionListener(Action) 
-		 
-		# Add a button to the gui for this category 
-		catPanel.add(button) 
+		if keyEvent.getKeyCode in listCode: 
+			self.selectedCategory = "theNewCat" 
+			self.doAction() 
+			 
 	 
-	# Save categories in memory
-	pref.put(ij.class, "listCat", listCat)
-
-	# Add Panel to WinButton
-	WinButton.addPanel(catPanel)
-	
-	# Add comment field 
-	WinButton.addStringField("Comments", "") 
-	
-	# Add default fields
-	WinButton.addDefaultOptions()
-	
-	WinButton.showDialog() 
+		  
+############### GUI - CATEGORY DIALOG - collect N classes names (N define at first line)  #############  
+  
+Win = GenericDialog("Categories names") 
+ 
+choice = ["a single category column", "1 column per category"] 
+indexDefault = pref.getInt("table_style", 0) 
+Win.addChoice("Classification table shoud have",  
+				choice,  
+				choice[indexDefault] ) 
+  
+# Add N string field to get class names 
+for i in range(N_category): 
+	listCat = pref.getList(ij.class, "listCat")            # try to retrieve the list of categories from the persistence, if not return [] - ij.class workaround see https://forum.image.sc/t/store-a-list-using-the-persistence-prefservice/26449 
+	 
+	if listCat and i<=len(listCat)-1: 
+		catName = listCat[i] 
+	else: 
+		catName = "Category_" + str(i+1) 
+	 
+	Win.addStringField("Category: ", catName) 
+	 
+	Win.addMessage("") # skip one line  
+	  
+Win.showDialog() 
+  
+  
+################# After OK clicking ###########  
+  
+# Recover fields from the formular  
+if (Win.wasOKed()):   
+ 
+	# get Choice single/multi column 
+	choiceIndex = Win.getNextChoiceIndex() 
+	pref.put("table_style", choiceIndex) 
+	 
+	tableTitle, Table = getTable() 
+	  
+	# Initialize GUI with category buttons  
+	winButton = ButtonDialog("Manual classifier - Single class per image", choiceIndex)  
+	winButton.addMessage("Click the category of the current image or ROI.\nTo annotate ROI, draw a ROI or activate one before clicking the category button.") 
+		 
+	# Loop over categories and add a button to the panel for each 
+	listCat = []  
+	catPanel = Panel(GridLayout(0,4)) # Unlimited number of rows - fix to 4 columns 
+	 
+	# Define actionListener for buttons: they share the same one, associated to the dialog
+	action = CatButtonAction(winButton)  
+	 
+	for i in range(N_category):  
+		  
+		# Recover the category name  
+		Cat = Win.getNextString()  
+		listCat.append(Cat)  
+  
+		# Create a Button  
+		button = Button(Cat) # button label 
+		  
+		# Bind action to button  
+		button.addActionListener(action)  
+		  
+		# Add a button to the gui for this category  
+		catPanel.add(button)  
+	  
+	# Save categories in memory 
+	pref.put(ij.class, "listCat", listCat) 
+ 
+	# Add Panel to winButton 
+	winButton.addPanel(catPanel) 
+	 
+	# Add comment field  
+	winButton.addStringField("Comments", "")  
+	 
+	# Add default fields 
+	winButton.addDefaultOptions() 
+	winButton.showDialog()  
