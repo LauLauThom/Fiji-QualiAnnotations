737778d211b1e44aea842b6cf03cb5888d087744
Laurent Thomas <laurent132.thomas@laposte.net>
2020-08-24 11:08:07 +0200

    support annotation of multiple rois

--- Fiji.app/jars/Lib/QualiAnnotations.py
+++ Fiji.app/jars/Lib/QualiAnnotations.py
@@ -1,15 +1,15 @@
-from ij 			  import IJ, WindowManager, Prefs
+from ij				  import IJ, WindowManager, Prefs
 from ij.plugin.filter import Analyzer
 from ij.plugin.frame  import RoiManager
-from ij.measure 	  import ResultsTable, Measurements
+from ij.measure		  import ResultsTable, Measurements
 import os
 from java.awt.event import ActionListener
-from fiji.util.gui  import GenericDialogPlus
+from fiji.util.gui	import GenericDialogPlus
 
 
 def getTable():
 	''' Check if a table called Annotations or Annotations.csv exists otherwise open a new one'''
-	win  = WindowManager.getWindow("Annotations")
+	win	 = WindowManager.getWindow("Annotations")
 	win2 = WindowManager.getWindow("Annotations.csv")
 	
 	if win: # different of None
@@ -27,6 +27,10 @@ def getTable():
 	return tableTitle, Table
 
 def getRoiManager():
+	"""
+	Return existing RoiManager or create a new instance if no existing one.
+	Always display the RoiManager
+	"""
 	rm = RoiManager.getInstance()
 	if not rm: rm = RoiManager() # create a new instance
 	return rm
@@ -57,15 +61,23 @@ def getImageDirAndName(imp, stackMode):
 			else: # 1D stack
 				filename = 'Slice ' + str(imp.currentSlice)	 
 				 
-		else :  
+		else :	
 			filename = filename.split('\n',1)[0] # can be useful when ImagesToStack/Import Sequence was used
 	
 	return directory, filename
 
 def nextSlice(imp):
-	if imp.isHyperStack(): imp.setT(imp.getT()+1)        # increment the time slider
+	if imp.isHyperStack(): imp.setT(imp.getT()+1)		 # increment the time slider
 	elif imp.isStack(): imp.setSlice(imp.currentSlice+1) # increment unique slider
 	else:pass
+
+def setRoiProperties(roi, table):
+	"""
+	Set the roi property as the value of the last table row 
+	"""
+	for heading in table.getHeadings():
+		value = table.getStringValue(heading, table.size()-1)
+		roi.setProperty(heading, value)
 
 
 class CustomDialog(GenericDialogPlus):
@@ -88,17 +100,15 @@ class CustomDialog(GenericDialogPlus):
 		
 			# Get stack mode
 			stackChoice = self.getChoices()[-1] # last dropdown
-			stackMode   = stackChoice.getSelectedItem()
+			stackMode	= stackChoice.getSelectedItem()
 			
 			# Check options
-			checkboxes  = self.getCheckboxes()
-			addRoi      = checkboxes[-3].getState()
-			doMeasure   = checkboxes[-2].getState()
-			doNext      = checkboxes[-1].getState()
+			checkboxes	= self.getCheckboxes()
+			doMeasure	= checkboxes[-2].getState()
+			doNext		= checkboxes[-1].getState()
 			
 			# Save them in preference
 			Prefs.set("annot.stackMode", stackMode)
-			Prefs.set("annot.addRoi", addRoi)
 			Prefs.set("annot.doMeasure", doMeasure)
 			Prefs.set("annot.doNext", doNext)
 			
@@ -119,7 +129,6 @@ class CustomDialog(GenericDialogPlus):
 		self.addChoice("Stack mode : 1 table entry per", choices, Prefs.get("annot.stackMode", "slice")) # add Presistence
 		
 		# Checkbox next slice and run Measure 
-		self.addCheckbox("Add ROI to Manager", bool(Prefs.get("annot.addRoi", True)) ) # force casting to boolean, as Prefs.get returns a float (might be jython speicifc)
 		self.addCheckbox("run 'Measure'", bool(Prefs.get("annot.doMeasure", False)) )
 		self.addCheckbox("Auto next slice", bool(Prefs.get("annot.doNext", True)) )
 		
@@ -128,7 +137,7 @@ class CustomDialog(GenericDialogPlus):
 		self.addMessage("Documentation and generic analysis workflows available on the GitHub repo (click Help)")
 		
 		# Add Help button pointing to the github
-		self.addHelp(r"https://github.com/LauLauThom/ImageJ-ManualClassifier")
+		self.addHelp(r"https://github.com/LauLauThom/Fiji-QualiAnnotations")
 
 		self.hideCancelButton()
 	
@@ -154,76 +163,105 @@ class CustomDialog(GenericDialogPlus):
 		+ it also calls the function fillTable which is implemented in descendant classes
 		DO NOT OVERWRITE
 		'''
-		imp = IJ.getImage() # get current image  				
+		imp = IJ.getImage() # get current image					
 		
 		# Get stack mode
 		stackChoice = self.getChoices()[-1] # last dropdown
-		stackMode   = stackChoice.getSelectedItem()
+		stackMode	= stackChoice.getSelectedItem()
 		
 		# Check options
-		checkboxes  = self.getCheckboxes()
-		addRoi      = checkboxes[-3].getState()
-		doMeasure   = checkboxes[-2].getState()
-		doNext      = checkboxes[-1].getState()
+		checkboxes	= self.getCheckboxes()
+		doMeasure	= checkboxes[-2].getState()
+		doNext		= checkboxes[-1].getState()
 		
 		# Get current table
 		tableTitle, Table = getTable()
 		Table.showRowNumbers(True)
 		
-		if doMeasure: # Automatically increment counter
-			analyzer = Analyzer(imp, Table)
-			analyzer.setMeasurement(Measurements.LABELS, False) # dont add label to table
-			analyzer.measure() # as selected in Set Measurements
-
-		else:
-			Table.incrementCounter() # Automatically done if doMeasure 
-		
 		# Recover image name  
 		directory, filename = getImageDirAndName(imp, stackMode)
-		#Table.addValue("Index", Table.getCounter() )  
-		Table.addValue("Folder", directory) 
-		Table.addValue("Image", filename)
-
-		# Add selected items (implementation-specific)
-		self.fillTable(Table)
- 
-		# Read comment 
-		stringField = self.getStringFields()[0] 
-		Table.addValue("Comment", stringField.text) 
 		
-		# Add Roi to RoiManager + set its properties
-		roi = imp.getRoi()
-		if roi: 
+		# Initialize Analyzer
+		if doMeasure:
+			analyzer = Analyzer(imp, Table)
+			analyzer.setMeasurement(Measurements.LABELS, False) # dont add label to table
+		
+		# Check if existing roi manager
+		rm = RoiManager.getInstance()
+		indexes = rm.getSelectedIndexes() if rm else [] # Check if roi selected
+		
+		if indexes:
 			
-			# Set properties of roi with new row
-			for heading in Table.getHeadings():
-				value = Table.getStringValue(heading, Table.size()-1)
-				roi.setProperty(heading, value)
+			# Loop over selected ROI
+			for index in indexes: # set selected features as property of rois
+				
+				roi = rm.getRoi(index)
+				imp.setRoi(roi)
+				
+				# Run measure for the ROI
+				if doMeasure: # Automatically increment counter
+					analyzer.measure() # as selected in Set Measurements
+					
+				else:
+					Table.incrementCounter() # Automatically done if doMeasure 
+				
+				#Table.addValue("Index", Table.getCounter() )  
+				Table.addValue("Folder", directory) 
+				Table.addValue("Image", filename)
+
+				# Add selected items (implementation-specific)
+				self.fillTable(Table)
+		 
+				# Read comment 
+				stringField = self.getStringFields()[0] 
+				Table.addValue("Comment", stringField.text)
+
+				# Add roi name to the table + set its property
+				Table.addValue("Roi", roi.getName()) # Add roi name to table
+				setRoiProperties(roi, Table)
+					
+		# No roi selected in the Manager
+		else:
+		
+			if doMeasure: # also automatically increment counter
+				analyzer.measure() # as selected in Set Measurements
+				
+			else:
+				Table.incrementCounter() # Automatically done if doMeasure 
 			
-			if addRoi:
-				
-				# Set Roi slice-position
-				roi.setPosition(imp)
-				
-				# Add to Manager
+			#Table.addValue("Index", Table.getCounter() )  
+			Table.addValue("Folder", directory) 
+			Table.addValue("Image", filename)
+
+			# Add selected items (implementation-specific)
+			self.fillTable(Table)
+	 
+			# Read comment 
+			stringField = self.getStringFields()[0] 
+			Table.addValue("Comment", stringField.text) 
+			
+			# Check if an active Roi, not yet present in Manager
+			roi = imp.getRoi()
+			
+			if roi is not None:
+				roi.setPosition(imp) 
 				rm = getRoiManager()
 				rm.addRoi(roi)
-				roiName = rm.getRoi(rm.getCount()-1).getName()
-			
-			else: # the ROI might belong to the ROI Manager
-				roiName = roi.getName()
-			
-			# Add roi name to table
-			Table.addValue("Roi", roiName)
-		 
-		Table.show(tableTitle) # Update table	    
+				
+				# get back the roi from the manager to set properties
+				roiBis	= rm.getRoi(rm.getCount()-1) 
+				roiName = roiBis.getName()
+				Table.addValue("Roi", roiName) # Add roi name to table
+				setRoiProperties(roiBis, Table)
+		
+		Table.show(tableTitle) # Update table		
 		#Table.updateResults() # only for result table but then addValue does not work !  
 		  
-		# Go to next slice  
+		# Go to next slice	
 		if doNext: nextSlice(imp)
 		  
 		# Bring back the focus to the button window (otherwise the table is in the front)  
-		if not IJ.getFullVersion().startswith("1.52p"): WindowManager.toFront(self)  # prevent some ImageJ bug with 1.52p
+		if not IJ.getFullVersion().startswith("1.52p"): WindowManager.toFront(self)	 # prevent some ImageJ bug with 1.52p
 
 
 class AddDialog(CustomDialog):
@@ -246,12 +284,12 @@ class AddDialog(CustomDialog):
 			self.doAction()
 		
 		
-class ButtonAction(ActionListener): # extends action listener   
+class ButtonAction(ActionListener): # extends action listener	
 	'''
 	Generic class used to defined button actions
 	The action is initialized with the dialog to be able to do stuff with it
 	- actionPerformed : Call when the button is clicked, here it calls the Action.main() passed to the constructor 
-	'''  
+	'''	 
 	
 	def __init__(self, dialog):
 		ActionListener.__init__(self)
