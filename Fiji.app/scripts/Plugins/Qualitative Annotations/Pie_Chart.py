51fea0e4906f1f476ac1938cbf3647a33b1efbb1
Laurent Thomas <laurent132.thomas@laposte.net>
2020-12-03 11:36:18 +0100

    Transform to class

--- Fiji.app/scripts/Plugins/Qualitative Annotations/Pie_Chart.py
+++ Fiji.app/scripts/Plugins/Qualitative Annotations/Pie_Chart.py
@@ -1,42 +1,47 @@
-# Pie Chart JFreeChart Jython Fiji 
-from javax.swing import JPanel , JFrame
+from javax.swing import JPanel, JFrame
 from org.jfree.chart import JFreeChart, ChartPanel, ChartFactory
-from org.jfree.data.general import DefaultPieDataset, PieDataset
+from org.jfree.data.general import DefaultPieDataset
 from org.jfree.ui import ApplicationFrame, RefineryUtilities 
-
 from collections import Counter
 
-test = ["1","1","1","2","2","3","3","3","3","3"] # convert to string otherwise issue
-counts = Counter(test)
-#print counts # value: counts OK
- 
-# Create a Pie dataset
-data = DefaultPieDataset() 
-for key, value in counts.items(): 
-	#print key, value 
-	data.setValue(key, value) 
- 
-print data 
- 
-# Create a JFreeChartInstance 
-title = "Category distribution"
-legend = True 
-tooltips = True 
-urls = False
-chart = ChartFactory.createPieChart(title, data, legend, tooltips, urls) 
-panel = ChartPanel(chart) 
- 
-class PieFrame(JFrame): 
-	 
-	def __init__(self, frameTitle):
-		JFrame.__init__(self, frameTitle)
-		self.setContentPane(panel)
+class PieChart(JFrame): 
+	"""
+	Create a Frame holding a PieChart from a list of data, typically a table column
+	Values in the list should be string
+	"""
+	
+	def __init__(self, plotTitle, listData, showLegend=True, showTooltip=True):
+		JFrame.__init__(self)
+		self.listData = listData
+		panel = self.generatePlotPanel(plotTitle, listData, showLegend, showTooltip)
+		self.setContentPane(panel) # put the plot panel in the JFrame
+
+	def generatePlotPanel(self, plotTitle, listData, showLegend, showTooltip):
+
+		# Get a dictionary of value occurence in the list {value1:count, value2:count...}
+		dataDico = Counter(listData)
+		#print dataDico # value: counts OK
+		 
+		# Create a Pie dataset from the dicoData
+		dataPlot = DefaultPieDataset() 
+		for key, value in dataDico.items(): 
+			#print key, value 
+			dataPlot.setValue(key, value) 
+				 
+		# Create a JFreeChartInstance
+		urls = False
+		chart = ChartFactory.createPieChart(plotTitle, dataPlot, showLegend, showTooltip, urls) 
 		
-	def showFrame(self):
+		return ChartPanel(chart)
+	
+	def showFrame(self, title, width=560, height=367):
 		"""Put the dimension in the show command"""
-		self.setSize( 560 , 367 )    
+		self.setTitle(title)
+		self.setSize(width, height)    
 		RefineryUtilities.centerFrameOnScreen(self)
 		self.setVisible(True)
-		
-frame = PieFrame("MyFirstFrame")
-frame.showFrame()
+
+if __name__ in ['__builtin__', '__main__']:
+	test = ["1","1","1","2","2","3","3","3","3","3"] # convert to string otherwise issue
+	frame = PieChart("My data column", test)
+	frame.showFrame("MyFrame")
