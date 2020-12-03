7e62882ea11056374400708402af7e1dfcfe4634
Laurent Thomas <laurent132.thomas@laposte.net>
2020-12-03 12:25:04 +0100

    plugin generating the PieChart

---
+++ Fiji.app/scripts/Plugins/Qualitative Annotations/Plot_PieChart.py
@@ -0,0 +1,30 @@
+from PieChart import PieChart
+from ij.gui import GenericDialog
+from ij import IJ, WindowManager
+
+tableWindow = WindowManager.getActiveTable()
+#print tableWindow
+
+if not tableWindow: IJ.error("No open table")
+
+# List column headers
+table   = tableWindow.getResultsTable()
+headers = table.getHeadings()
+
+# Generate dialog with dropdown for column selection
+dialog = GenericDialog("PieChart from table column")
+dialog.addChoice("Data column", headers, headers[0])
+dialog.showDialog()
+
+# Generate PieChart with data column
+if dialog.wasOKed():
+
+	# Get the data column as string
+	selectedHeader = dialog.getNextChoice()
+	column         = table.getColumnAsVariables(selectedHeader)
+	columnString   = [str(item) for item in column]
+
+	# Make the PieChart for this data column
+	if columnString:
+		chart = PieChart(selectedHeader, columnString)
+		chart.showFrame("PieChart")
