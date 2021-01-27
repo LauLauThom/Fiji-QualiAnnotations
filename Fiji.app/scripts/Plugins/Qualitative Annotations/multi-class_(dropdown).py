f9e8014d8540b2c0b0e9838ff2b714e8c4e793ab
Laurent Thomas <laurent132.thomas@laposte.net>
2021-01-27 11:13:45 +0100

    add docstring with default csv

--- Fiji.app/scripts/Plugins/Qualitative Annotations/multi-class_(dropdown).py
+++ Fiji.app/scripts/Plugins/Qualitative Annotations/multi-class_(dropdown).py
@@ -11,6 +11,7 @@ Broken, ,
 '''
 #@ File   (label="CSV file for category and choice", style="extension:csv") csvpath
 #@ String (label="Browsing mode", choices={"stack", "directory"}) browse_mode
+#@ String (visibility=MESSAGE, value="Example of input csv at Fiji.app/lib/QualiAnnotations-ExampleDropDown.csv", required=false) comment
 from java.awt 		import Panel, Choice, Label, GridLayout
 from fiji.util.gui	import GenericDialogPlus
 from QualiAnnotations import CustomDialog
