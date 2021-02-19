ab5ce752fa1357b523a5c18d0b43e49c203bfb64
Laurent Thomas <laurent132.thomas@laposte.net>
2021-02-19 12:21:58 +0100

    add BarChart

--- Fiji.app/jars/Lib/PieChart.py
+++ Fiji.app/jars/Lib/PieChart.py
@@ -1,7 +1,9 @@
-from javax.swing            import JPanel, JFrame
-from org.jfree.chart.plot   import PiePlot
-from org.jfree.chart        import JFreeChart, ChartPanel, ChartFactory
-from org.jfree.data.general import DefaultPieDataset
+from javax.swing             import JPanel, JFrame
+from org.jfree.chart.plot    import PiePlot
+from org.jfree.chart         import JFreeChart, ChartPanel, ChartFactory
+from org.jfree.data.general  import DefaultPieDataset
+from org.jfree.data.category import DefaultCategoryDataset; 
+
 from org.jfree.ui import ApplicationFrame, RefineryUtilities 
 from collections import Counter
 
@@ -52,9 +54,58 @@ class PieChart(JFrame):
 		RefineryUtilities.centerFrameOnScreen(self)
 		self.setVisible(True)
 
+
+class BarChart(JFrame): 
+	"""
+	Create a Frame holding a BarChart from a list of data, typically a table column
+	Values in the data list should be string
+	Inspired from https://www.tutorialspoint.com/jfreechart/jfreechart_pie_chart.htm
+	Except that the class extends on JFrame and not ApplicationFrame, which causes Fiji to quit completely when plot is closed
+	"""
+	
+	def __init__(self, plotTitle, listData, showLegend=True, showTooltip=True):
+		JFrame.__init__(self)
+		self.listData = listData
+		panel = self.generatePlotPanel(plotTitle, listData)
+		self.setContentPane(panel) # put the plot panel in the JFrame
+
+	def generatePlotPanel(self, plotTitle, listData):
+		"""
+		1) Create a CategoryDataset
+		2) Create a BarChart (or  Create a BarPlot and put it in a JFreeChart)
+		3) Put the BarChart in a ChartPanel
+		"""
+		# Get a dictionary of value occurence in the list {value1:count, value2:count...}
+		dataDico = Counter(listData)
+		#print dataDico # value: counts OK
+		 
+		# Create a Pie dataset from the dicoData
+		dataset = DefaultCategoryDataset() 
+		for key, value in dataDico.items(): 
+			#print key, value 
+			dataset.setValue(value, key, "") 
+		
+		# Create an instance of JFreeChart 	
+		chart = ChartFactory.createBarChart(plotTitle, "Categories", "Count", dataset) 
+		# Alternative way
+		#piePlot = PiePlot(pieDataset)
+		#chart   = JFreeChart(plotTitle, piePlot)
+		
+		return ChartPanel(chart)
+	
+	def showFrame(self, title, width=560, height=367):
+		"""Put the dimension in the show command"""
+		self.setTitle(title)
+		self.setSize(width, height)    
+		RefineryUtilities.centerFrameOnScreen(self)
+		self.setVisible(True)
+
 if __name__ in ['__builtin__', '__main__']:
 
 	# Create toy dataset and display the pie plot
 	data = ["banana"]*3 + ["apple"]*5 + ["cherry"]*10
 	frame = PieChart("Fruits", data)
-	frame.showFrame("MyFrame")
+	frame.showFrame("My Pie chart")
+	
+	frame2 = BarChart("Fruits", data)
+	frame2.showFrame("My Bar chart")
