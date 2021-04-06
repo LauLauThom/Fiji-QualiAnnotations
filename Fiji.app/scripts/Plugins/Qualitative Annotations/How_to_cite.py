e049e8291ba204059d77c2bfeadee1c4e0b4e9b0
Laurent Thomas <laurent132.thomas@laposte.net>
2021-04-06 15:41:12 +0200

    Help button renamed "Article page"

--- Fiji.app/scripts/Plugins/Qualitative Annotations/How_to_cite.py
+++ Fiji.app/scripts/Plugins/Qualitative Annotations/How_to_cite.py
@@ -7,12 +7,11 @@ gd.addMessage("""If you use these plugins, please cite :
 		Fiji plugins for qualitative image annotations: routine analysis and application to image classification
 		[version 2; peer review: 2 approved, 1 approved with reservations]. 
 		F1000Research 2021, 9:1248 
-		doi : 10.12688/f1000research.26872.2
-		
-		Click the help button below to open the article page""")
+		doi : 10.12688/f1000research.26872.2""")
 
 #URL = r"https://doi.org/10.12688/f1000research.26872.1/" # mind the last slash at the end of URL, otherwise not working
 URL = r"https://f1000research.com/articles/9-1248"
 gd.addHelp(URL)
+gd.setHelpLabel("Article page")
 gd.hideCancelButton()
 gd.showDialog()
